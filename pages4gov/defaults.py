2abec68c73bfc0e89b4dde5c0120f433a033520a
Simone Dalla <simodalla@gmail.com>
2014-10-29 10:27:22 +0100

    work in progress on consiglio comunale event`

--- pages4gov/defaults.py
+++ pages4gov/defaults.py
@@ -1,4 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
+from mezzanine.conf import register_setting
 
+
+register_setting(
+    name="PAGES4GOV_YOUTUBE_URL_QUERY",
+    description="Specifica i parametri per la URL di YouTube.",
+    editable=True,
+    default='autoplay=1&origin=',
+)
+
+register_setting(
+    name="PAGES4GOV_YOUTUBE_EMBED_WIDTH",
+    description="",
+    editable=True,
+    default='640',
+)
+
+register_setting(
+    name="PAGES4GOV_YOUTUBE_EMBED_HEIGHT",
+    description="",
+    editable=True,
+    default='390',
+)
--- pages4gov/views.py
+++ pages4gov/views.py
@@ -6,7 +6,23 @@ from django.views.generic.base import TemplateView
 from django.views.generic.dates import YearArchiveView, MonthArchiveView
 from django.views.generic.list import ListView
 
+from mezzanine.conf import settings
+
 from .models import ConsiglioComunaleEvent
+
+
+class SettingsMixin(object):
+
+    context_app_settings = []
+
+    def get_context_data(self, **kwargs):
+        context = super(SettingsMixin, self).get_context_data(**kwargs)
+        context.update({'app_settings': {}})
+        settings.use_editable()
+        for setting in self.context_app_settings:
+            context['app_settings'].update(
+                {setting.upper(): getattr(settings, setting, '')})
+        return context
 
 
 class EventiIstituzionaliHomeView(TemplateView):
@@ -33,6 +49,9 @@ class ConsiglioComunaleMonthArchiveView(MonthArchiveView):
     allow_empty = True
 
 
-class ConsiglioComunaleDetailView(DetailView):
+class ConsiglioComunaleDetailView(SettingsMixin, DetailView):
+    context_app_settings = ['PAGES4GOV_YOUTUBE_URL_QUERY',
+                            'PAGES4GOV_YOUTUBE_EMBED_WIDTH',
+                            'PAGES4GOV_YOUTUBE_EMBED_HEIGHT']
     model = ConsiglioComunaleEvent
 
